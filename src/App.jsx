8e8861aa1ce5f3a91f01fb6b594c6e03cccd3632
lucas Bollella <lubolle@gmail.com>
2023-01-13 16:29:48 -0300

    Creo los dos primeros componentes del sitio principal

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import './categories.styles.scss';
+import CategoryMenu from './components/categroy-menu/category-menu.component';
+
+const App = () => {
+
+  return (
+    <CategoryMenu />
+  )
+}
+
+export default App;
